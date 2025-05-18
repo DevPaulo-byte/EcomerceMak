bb4768dc0122ef9b7467329efbfa2d6762badf67
DevPaulo-byte <paulosants867@gmail.com>
2025-05-18 13:38:12 -0300

    novas alteraçoes

--- backend/database.php
+++ backend/database.php
@@ -1,60 +1,10 @@
 <?php
 
-namespace Database;
+$db = new mysqli("localhost", "root", "paulo29", "ecomerce", "3306");
 
-class Database
-{
-    private $password;
-    private $driver;
-    private $host;
-    private $dbname;
-    private $username;
-
-    private $con;
-
-    public function __construct()
-    {
-        $this->driver = 'mysql';
-        $this->host = 'localhost';
-        $this->dbname = 'ecomerce';
-        $this->username = 'root';
-        $this->password = 'paulo29';
-
-        try {
-            $this->con = new \PDO(
-                "$this->driver:host=$this->host;dbname=$this->dbname;charset=utf8",
-                $this->username,
-                $this->password
-            );
-
-            $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch (\PDOException $e) {
-            echo "<p style='color: red;'>❌ Erro na conexão: " . $e->getMessage() . "</p>";
-        }
-    }
-
-
-
-    public function getConnection()
-    {
-        return $this->con;
-    }
-
-// Método para exibir a mensagem de sucesso de conexão, quando necessário
-    public function showConnectionStatus()
-    {
-        if ($this->con) {
-            echo "<p style='color: green;'>✅ Conexão com o banco de dados estabelecida com sucesso!</p>";
-        }
-    }
+if ($db->connect_error) {
+    echo("Connection failed: {$db->connect_error}");
+    exit();
 }
 
-// include 'database.php';
-
-// $db = new database();
-
-// if ($db->getConnection()) {
-//     echo "Conexão bem-sucedida!";
-// } else {
-//     echo "Falha na conexão!";
-// }
+echo("Connected successfully");
